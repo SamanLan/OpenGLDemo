a1070fa93c5a695cbcd409ab4a99075d7493f81c
LanSaman <lansaman@163.com>
2018-04-16 21:28:13 +0800

    day2：完善helper

--- app/src/main/java/com/samanlan/opengldemo/CustomRenderer.java
+++ app/src/main/java/com/samanlan/opengldemo/CustomRenderer.java
@@ -22,10 +22,16 @@ public class CustomRenderer implements GLSurfaceView.Renderer {
 
     // float32位精度，一个byte8位精度，每个浮点数占4个字节
     private static final int BYTE_FOR_FLOAT = 4;
+    // 表明使用两个分量表示与一个顶点关联。但a_position被定义为vec4，它有四个分量，在默认情况下前三个设为0，最后一个设为1
+    private static final int POSITION_COMPONENT_COUNT = 2;
     // 用来在本地内存存储数据
     private final FloatBuffer mVertexData;
-
+    private int programId;
     private Context mContext;
+    private static final String U_COLOR = "u_color";
+    private static final String A_POSITION = "a_position";
+    private int uColorLocation;
+    private int aPosition;
 
     public CustomRenderer(Context context) {
         mContext = context;
@@ -39,12 +45,6 @@ public class CustomRenderer implements GLSurfaceView.Renderer {
                 .allocateDirect(tableVertices.length * BYTE_FOR_FLOAT)// 分配本地内存，不会被gc回收
                 .order(ByteOrder.nativeOrder())// 告诉字节缓冲区按照本地字节序组织它的内容
                 .asFloatBuffer();
-
-        // 顶点着色器vertex shader。生成每个顶点的最终位置，针对每个顶点它都会执行一次。
-        int vertexShader = GLHelper.compileShader(GLES20.GL_VERTEX_SHADER, GLHelper.readTextFileFromResource(mContext, R.raw.custom_vertex_shader));
-
-        // 片段着色器fragment shader。为组成点、直线或者三角形的每个片段生成最终的颜色，针对每个片段它都会执行一次。
-        int fragmentShader = GLHelper.compileShader(GLES20.GL_FRAGMENT_SHADER, GLHelper.readTextFileFromResource(mContext, R.raw.custom_fragment_shader));
     }
 
     @Override
@@ -52,6 +52,32 @@ public class CustomRenderer implements GLSurfaceView.Renderer {
         // 用这个颜色清空屏幕，参数对应rgba
         GLES20.glClearColor(1f, 0f, 0f, 0f);
 
+        // 顶点着色器vertex shader。生成每个顶点的最终位置，针对每个顶点它都会执行一次。
+        int vertexShader = GLHelper.compileShader(GLES20.GL_VERTEX_SHADER, GLHelper.readTextFileFromResource(mContext, R.raw.custom_vertex_shader));
+
+        // 片段着色器fragment shader。为组成点、直线或者三角形的每个片段生成最终的颜色，针对每个片段它都会执行一次。
+        int fragmentShader = GLHelper.compileShader(GLES20.GL_FRAGMENT_SHADER, GLHelper.readTextFileFromResource(mContext, R.raw.custom_fragment_shader));
+
+        programId = GLHelper.linkProgram(vertexShader, fragmentShader);
+
+        GLHelper.validateProgram(programId);
+
+        GLES20.glUseProgram(programId);
+
+        // 获取uinform位置并存入uColorLocation
+        uColorLocation = GLES20.glGetUniformLocation(programId, U_COLOR);
+
+        // 获取属性位置
+        // 在使用属性之前也要获取它的位置，我们可以让OpenGL自动给这些属性分配位置编号，或者在着色器被链接到一起的之前，可以通过
+        // 调用glBindAttribLocation由我们给它们分配位置编号
+        aPosition = GLES20.glGetAttribLocation(programId, A_POSITION);
+
+        // 关联属性与顶点数据的数组
+        // 确保从开头开始读取数据
+        mVertexData.position(0);
+        GLES20.glVertexAttribPointer(aPosition, POSITION_COMPONENT_COUNT, GLES20.GL_FLOAT, false, 0, mVertexData);
+        // 使顶点数组能使用
+        GLES20.glEnableVertexAttribArray(aPosition);
 
     }
 
@@ -65,5 +91,10 @@ public class CustomRenderer implements GLSurfaceView.Renderer {
     public void onDrawFrame(GL10 gl10) {
         // 擦除屏幕所有颜色，并用之前glClearColor定义的颜色填充屏幕
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+        // 绘制
+        GLES20.glUniform4f(uColorLocation, 1f, 1f, 1f, 1f);
+        // 告诉OpenGL我们想画三角形，从顶点的开头处开始读，读入6个顶点，最终会画出两个三角形
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 6);
     }
 }
--- app/src/main/java/com/samanlan/opengldemo/GLHelper.java
+++ app/src/main/java/com/samanlan/opengldemo/GLHelper.java
@@ -44,6 +44,7 @@ public class GLHelper {
         // 创建一个新的着色器对象
         final int shaderObjectId = GLES20.glCreateShader(type);
         if (shaderObjectId==0) {
+            System.out.println("创建着色器失败");
             return 0;
         }
         // 有了对象就上传源代码，将对象与代码关联起来
@@ -52,10 +53,48 @@ public class GLHelper {
         // 读取与shaderObjectId关联的编译状态，并将它写入到compileStatus的第0个元素
         GLES20.glGetShaderiv(shaderObjectId, GLES20.GL_COMPILE_STATUS, compileStatus, 0);
         if (compileStatus[0] == 0) {
+            System.out.println("编译失败，原因：" + GLES20.glGetShaderInfoLog(shaderObjectId));
             // 编译失败删除shader对象
             GLES20.glDeleteShader(shaderObjectId);
             return 0;
         }
         return shaderObjectId;
+    }
+
+    public static int linkProgram(int vertexShaderId, int fragmentShaderId) {
+        // 新建程序对象
+        final int progamObjectId = GLES20.glCreateProgram();
+        if (progamObjectId == 0) {
+            System.out.println("创建工程失败");
+            return 0;
+        }
+
+        // 附着上着色器
+        GLES20.glAttachShader(progamObjectId, vertexShaderId);
+        GLES20.glAttachShader(progamObjectId, fragmentShaderId);
+
+        // 链接program
+        GLES20.glLinkProgram(progamObjectId);
+
+        final int[] linkStatus = new int[1];
+        // 读取与progamObjectId关联的连接状态，并将它写入到linkStatus的第0个元素
+        GLES20.glGetProgramiv(progamObjectId, GLES20.GL_LINK_STATUS, linkStatus, 0);
+        if (linkStatus[0] == 0) {
+            System.out.println("连接program失败，原因：" + GLES20.glGetProgramInfoLog(progamObjectId));
+            // 连接失败则删除program对象
+            GLES20.glDeleteProgram(progamObjectId);
+            return 0;
+        }
+        return progamObjectId;
+    }
+
+    public static boolean validateProgram(int programObjectId) {
+        // 验证一下OpenGL程序是否有效，是否低效率、无法运行等
+        // 调试期开启即可
+        GLES20.glValidateProgram(programObjectId);
+        final int[] validateStatus = new int[1];
+        GLES20.glGetProgramiv(programObjectId, GLES20.GL_VALIDATE_STATUS, validateStatus, 0);
+        System.out.println("验证消息：" + GLES20.glGetProgramInfoLog(programObjectId));
+        return validateStatus[0] != 0;
     }
 }
